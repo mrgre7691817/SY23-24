5b7b2d5bc84892cf26f47f2b392a2250f334c578
Marissa <mrgre558@learner.dvusd.org>
2024-03-21 12:35:33 -0700

    Jewelry ICP Code

--- Jewlery/Jewlery/Form1.Designer.vb
+++ Jewlery/Jewlery/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,9 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.SilverPB = New System.Windows.Forms.PictureBox()
-        Me.GoldPB = New System.Windows.Forms.PictureBox()
         Me.RoseGoldPB = New System.Windows.Forms.PictureBox()
         Me.EmeraldPB = New System.Windows.Forms.PictureBox()
         Me.GarnetPB = New System.Windows.Forms.PictureBox()
@@ -32,21 +31,27 @@ Partial Class Form1
         Me.AmethystPB = New System.Windows.Forms.PictureBox()
         Me.MetalCB = New System.Windows.Forms.ComboBox()
         Me.JewelCB = New System.Windows.Forms.ComboBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.CostTB = New System.Windows.Forms.TextBox()
+        Me.LengthTB = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Total = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.GoldPB = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.JewelCTB = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SizeTB = New System.Windows.Forms.TextBox()
         CType(Me.SilverPB, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.GoldPB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RoseGoldPB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.EmeraldPB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GarnetPB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DiamondPB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.AquamarinePB, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.AmethystPB, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GoldPB, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'SilverPB
@@ -57,24 +62,17 @@ Partial Class Form1
         Me.SilverPB.Size = New System.Drawing.Size(205, 199)
         Me.SilverPB.TabIndex = 0
         Me.SilverPB.TabStop = False
-        '
-        'GoldPB
-        '
-        Me.GoldPB.Image = Global.Jewlery.My.Resources.Resources.Gold
-        Me.GoldPB.Location = New System.Drawing.Point(32, 485)
-        Me.GoldPB.Name = "GoldPB"
-        Me.GoldPB.Size = New System.Drawing.Size(202, 197)
-        Me.GoldPB.TabIndex = 1
-        Me.GoldPB.TabStop = False
+        Me.SilverPB.Visible = False
         '
         'RoseGoldPB
         '
         Me.RoseGoldPB.Image = Global.Jewlery.My.Resources.Resources.rosegold
-        Me.RoseGoldPB.Location = New System.Drawing.Point(32, 484)
+        Me.RoseGoldPB.Location = New System.Drawing.Point(50, 484)
         Me.RoseGoldPB.Name = "RoseGoldPB"
         Me.RoseGoldPB.Size = New System.Drawing.Size(203, 197)
         Me.RoseGoldPB.TabIndex = 2
         Me.RoseGoldPB.TabStop = False
+        Me.RoseGoldPB.Visible = False
         '
         'EmeraldPB
         '
@@ -85,50 +83,56 @@ Partial Class Form1
         Me.EmeraldPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.EmeraldPB.TabIndex = 3
         Me.EmeraldPB.TabStop = False
+        Me.EmeraldPB.Visible = False
         '
         'GarnetPB
         '
         Me.GarnetPB.Image = Global.Jewlery.My.Resources.Resources.Garnet
-        Me.GarnetPB.Location = New System.Drawing.Point(32, 295)
+        Me.GarnetPB.Location = New System.Drawing.Point(50, 295)
         Me.GarnetPB.Name = "GarnetPB"
         Me.GarnetPB.Size = New System.Drawing.Size(152, 162)
         Me.GarnetPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.GarnetPB.TabIndex = 4
         Me.GarnetPB.TabStop = False
+        Me.GarnetPB.Visible = False
         '
         'DiamondPB
         '
         Me.DiamondPB.Image = Global.Jewlery.My.Resources.Resources.Diamonds
-        Me.DiamondPB.Location = New System.Drawing.Point(30, 295)
+        Me.DiamondPB.Location = New System.Drawing.Point(70, 295)
         Me.DiamondPB.Name = "DiamondPB"
         Me.DiamondPB.Size = New System.Drawing.Size(150, 162)
         Me.DiamondPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.DiamondPB.TabIndex = 5
         Me.DiamondPB.TabStop = False
+        Me.DiamondPB.Visible = False
         '
         'AquamarinePB
         '
         Me.AquamarinePB.Image = Global.Jewlery.My.Resources.Resources.Aquamarine
-        Me.AquamarinePB.Location = New System.Drawing.Point(32, 295)
+        Me.AquamarinePB.Location = New System.Drawing.Point(101, 295)
         Me.AquamarinePB.Name = "AquamarinePB"
         Me.AquamarinePB.Size = New System.Drawing.Size(152, 162)
         Me.AquamarinePB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.AquamarinePB.TabIndex = 6
         Me.AquamarinePB.TabStop = False
+        Me.AquamarinePB.Visible = False
         '
         'AmethystPB
         '
         Me.AmethystPB.Image = Global.Jewlery.My.Resources.Resources.Amethyst
-        Me.AmethystPB.Location = New System.Drawing.Point(30, 295)
+        Me.AmethystPB.Location = New System.Drawing.Point(126, 295)
         Me.AmethystPB.Name = "AmethystPB"
         Me.AmethystPB.Size = New System.Drawing.Size(152, 162)
         Me.AmethystPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.AmethystPB.TabIndex = 7
         Me.AmethystPB.TabStop = False
+        Me.AmethystPB.Visible = False
         '
         'MetalCB
         '
         Me.MetalCB.FormattingEnabled = True
+        Me.MetalCB.Items.AddRange(New Object() {"Gold ", "Silver", "Rose Gold"})
         Me.MetalCB.Location = New System.Drawing.Point(50, 58)
         Me.MetalCB.Name = "MetalCB"
         Me.MetalCB.Size = New System.Drawing.Size(121, 24)
@@ -137,24 +141,25 @@ Partial Class Form1
         'JewelCB
         '
         Me.JewelCB.FormattingEnabled = True
+        Me.JewelCB.Items.AddRange(New Object() {"Emrald", "Garnet", "Amethyst", "Diamond", "Aquamarine"})
         Me.JewelCB.Location = New System.Drawing.Point(251, 58)
         Me.JewelCB.Name = "JewelCB"
         Me.JewelCB.Size = New System.Drawing.Size(121, 24)
         Me.JewelCB.TabIndex = 9
         '
-        'TextBox1
+        'CostTB
         '
-        Me.TextBox1.Location = New System.Drawing.Point(725, 58)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 10
+        Me.CostTB.Location = New System.Drawing.Point(725, 58)
+        Me.CostTB.Name = "CostTB"
+        Me.CostTB.Size = New System.Drawing.Size(100, 22)
+        Me.CostTB.TabIndex = 10
         '
-        'TextBox2
+        'LengthTB
         '
-        Me.TextBox2.Location = New System.Drawing.Point(101, 246)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(91, 22)
-        Me.TextBox2.TabIndex = 11
+        Me.LengthTB.Location = New System.Drawing.Point(101, 246)
+        Me.LengthTB.Name = "LengthTB"
+        Me.LengthTB.Size = New System.Drawing.Size(91, 22)
+        Me.LengthTB.TabIndex = 11
         '
         'Label1
         '
@@ -168,11 +173,11 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(672, 64)
+        Me.Label2.Location = New System.Drawing.Point(636, 61)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(34, 16)
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
         Me.Label2.TabIndex = 13
-        Me.Label2.Text = "Cost"
+        Me.Label2.Text = "Chain cost/in"
         '
         'Label3
         '
@@ -192,27 +197,84 @@ Partial Class Form1
         Me.Label4.TabIndex = 15
         Me.Label4.Text = "Jewels"
         '
+        'Total
+        '
+        Me.Total.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Total.Location = New System.Drawing.Point(725, 114)
+        Me.Total.Name = "Total"
+        Me.Total.ReadOnly = True
+        Me.Total.Size = New System.Drawing.Size(99, 22)
+        Me.Total.TabIndex = 16
+        '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(612, 312)
+        Me.Label5.Location = New System.Drawing.Point(672, 120)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(48, 16)
-        Me.Label5.TabIndex = 16
-        Me.Label5.Text = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 16)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Total" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'GoldPB
+        '
+        Me.GoldPB.Image = Global.Jewlery.My.Resources.Resources.Gold
+        Me.GoldPB.Location = New System.Drawing.Point(70, 484)
+        Me.GoldPB.Name = "GoldPB"
+        Me.GoldPB.Size = New System.Drawing.Size(203, 199)
+        Me.GoldPB.TabIndex = 18
+        Me.GoldPB.TabStop = False
+        Me.GoldPB.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(615, 30)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(104, 16)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Jewel cost/carat" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'JewelCTB
+        '
+        Me.JewelCTB.Location = New System.Drawing.Point(725, 30)
+        Me.JewelCTB.Name = "JewelCTB"
+        Me.JewelCTB.Size = New System.Drawing.Size(99, 22)
+        Me.JewelCTB.TabIndex = 20
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 221)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 16)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Size (mm)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'SizeTB
+        '
+        Me.SizeTB.Location = New System.Drawing.Point(101, 221)
+        Me.SizeTB.Name = "SizeTB"
+        Me.SizeTB.Size = New System.Drawing.Size(91, 22)
+        Me.SizeTB.TabIndex = 22
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1192, 758)
+        Me.Controls.Add(Me.SizeTB)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.JewelCTB)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.GoldPB)
         Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Total)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.LengthTB)
+        Me.Controls.Add(Me.CostTB)
         Me.Controls.Add(Me.JewelCB)
         Me.Controls.Add(Me.MetalCB)
         Me.Controls.Add(Me.AmethystPB)
@@ -221,25 +283,23 @@ Partial Class Form1
         Me.Controls.Add(Me.GarnetPB)
         Me.Controls.Add(Me.EmeraldPB)
         Me.Controls.Add(Me.RoseGoldPB)
-        Me.Controls.Add(Me.GoldPB)
         Me.Controls.Add(Me.SilverPB)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.SilverPB, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.GoldPB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.RoseGoldPB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.EmeraldPB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GarnetPB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DiamondPB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.AquamarinePB, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.AmethystPB, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GoldPB, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents SilverPB As PictureBox
-    Friend WithEvents GoldPB As PictureBox
     Friend WithEvents RoseGoldPB As PictureBox
     Friend WithEvents EmeraldPB As PictureBox
     Friend WithEvents GarnetPB As PictureBox
@@ -248,11 +308,17 @@ Partial Class Form1
     Friend WithEvents AmethystPB As PictureBox
     Friend WithEvents MetalCB As ComboBox
     Friend WithEvents JewelCB As ComboBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents CostTB As TextBox
+    Friend WithEvents LengthTB As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents Total As TextBox
     Friend WithEvents Label5 As Label
+    Friend WithEvents GoldPB As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents JewelCTB As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents SizeTB As TextBox
 End Class
--- Jewlery/Jewlery/Form1.vb
+++ Jewlery/Jewlery/Form1.vb
@@ -1,3 +1,70 @@
 ﻿Public Class Form1
+    Private Sub Form1_TextChanged(sender As Object, e As EventArgs) Handles Me.TextChanged
+        Dim l As Decimal
+        Dim s As Decimal
+        Dim c As Decimal
+        Dim jc As Decimal
+        Decimal.TryParse(LengthTB.Text, l)
+        Decimal.TryParse(CostTB.Text, c)
+        Decimal.TryParse(SizeTB.Text, s)
+        Decimal.TryParse(JewelCTB.Text, jc)
+        Total.Text = TotalCost(l, c, s, jc)
+    End Sub
+    Function TotalCost(Length As Decimal, Cost As Decimal, Size As Decimal, JCost As Decimal) As Decimal
+        Return (Length * Cost) + (JCost * Size)
+    End Function
 
+    Private Sub MetalCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles MetalCB.SelectedIndexChanged
+        If MetalCB.Text = "Gold " Then
+            GoldPB.Visible = True
+            CostTB.Text = "2.99"
+        Else
+            GoldPB.Visible = False
+        End If
+        If MetalCB.Text = "Silver" Then
+            SilverPB.Visible = True
+            CostTB.Text = "1.50"
+        Else
+            SilverPB.Visible = False
+        End If
+        If MetalCB.Text = "Rose Gold" Then
+            RoseGoldPB.Visible = True
+            CostTB.Text = "1.00"
+        Else
+            RoseGoldPB.Visible = False
+        End If
+    End Sub
+
+    Private Sub JewelCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles JewelCB.SelectedIndexChanged
+        If JewelCB.Text = "Emerald" Then
+            EmeraldPB.Visible = True
+            JewelCTB.Text = "350"
+        Else
+            EmeraldPB.Visible = False
+        End If
+        If JewelCB.Text = "Garnet" Then
+            GarnetPB.Visible = True
+            JewelCTB.Text = "550"
+        Else
+            GarnetPB.Visible = False
+        End If
+        If JewelCB.Text = "Amethyst" Then
+            AmethystPB.Visible = True
+            JewelCTB.Text = "50"
+        Else
+            AmethystPB.Visible = False
+        End If
+        If JewelCB.Text = "Diamond" Then
+            DiamondPB.Visible = True
+            JewelCTB.Text = "3400"
+        Else
+            DiamondPB.Visible = False
+        End If
+        If JewelCB.Text = "Aquamarine" Then
+            AquamarinePB.Visible = True
+            JewelCTB.Text = "675"
+        Else
+            AquamarinePB.Visible = False
+        End If
+    End Sub
 End Class
